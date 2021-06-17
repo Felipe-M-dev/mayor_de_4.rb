5e1d1a21c12b4ba9388a606b95f046bd225e05d0
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2021-06-17 14:35:32 -0400

    Update mayor_de_4.rb

--- mayor_de_4.rb
+++ mayor_de_4.rb
@@ -1,5 +1,6 @@
 #Inicio
 #Ingresar 3 números de cualquier valor
+#Valida que al menos sean 4 parametros con nil
 #Si n_1 es mayor que n_2, n_3 y n_4 muestra n_1
 #Si n_2 es mayor que n_3 y n_4 muestra n_2
 #Si n_3 es mayor que n_4
@@ -8,19 +9,23 @@
 
 ## Args
 
-n_1 = ARGV[0].to_i
-n_2 = ARGV[1].to_i
-n_3 = ARGV[2].to_i
-n_4 = ARGV[3].to_i
+n_1 = ARGV[0]
+n_2 = ARGV[1]
+n_3 = ARGV[2]
+n_4 = ARGV[3]
 
 ## Main
 
-if n_1 > n_2 && n_1 > n_3 && n_1 > n_4
-    puts n1
-elsif n_2 > n_3 && n_2 > n_4
-    puts n2
-elsif n_3 > n_4
-    puts n_3
+if n_4.nil?
+    puts "Ingrese 4 parametros"
 else
-    puts n_4
+    if n_1.to_i > n_2.to_i && n_1.to_i > n_3.to_i && n_1.to_i > n_4.to_i
+        puts n1
+    elsif n_2.to_i > n_3.to_i && n_2.to_i > n_4.to_i
+        puts n2
+    elsif n_3.to_i > n_4.to_i
+        puts n_3
+    else
+        puts n_4
+    end
 end
